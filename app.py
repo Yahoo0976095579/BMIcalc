e14197ff4d8ae7dfd639dfae66b156b12eca72de
李柏駿 <3b017012@gm.student.ncut.edu.tw>
2025-03-15 11:30:52 +0800

    Add print statement and vscode setting

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ height = input('請輸入身高(cm): ')
 weight = input('請輸入體重(kg): ')
 bmi = int(weight) / (int(height) / 100)**2
 print('您的 BMI 值為:', bmi)
+print("your weight:"+str(weight))
+print("your bmi:"+str(round(bmi,2)))
